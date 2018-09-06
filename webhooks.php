2c7f535ee36c9b6a760c722aff02eeb1b15796a2
zza20123 <phitthaya_0908@hotmail.com>
2018-09-06 09:58:29 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'CuFR5ZiKy2sbFsJGFWmg6FTJ / TisPc19JV / IzCjql3o8btPdk2q25MvZaITc1BJnRsR703gwlQCm + + BYeUXRfWT6vrURmGIlPDAQsVl7oylJiM40DD4j4DpKKefkCiRtikFQFp6I9NzGJC0Q vDu7kAdB04t89 / 1O / w1cDnyilFU ='
 
 // Get POST body content
 $content = file_get_contents('php://input');
